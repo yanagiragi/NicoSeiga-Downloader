d2c2b6dd2b3aa58fe0a0451d70039352efff70d9
yanagiragi <qaz90473@gmail.com>
2016-08-13 07:22:18 +0800

    early break when cookie not succuessly created

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var request = require('request');
 const cookie = require('cookie');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const querystring = require('querystring');
+
 const login = require('./data.js').getUrl();
 const options = require('./data.js').getAuth();
 
@@ -13,29 +13,28 @@ start(); // Main
 function start(){
 	auth(login, options)
 		.then(jar => storecookie(jar))
-		.then((str) => {
-			test(str);
+		.then(() => {
+			var thx = 'http://live.nicovideo.jp/api/getplayerstatus/nsen/vocaloid';
+			var thx2 = 'http://seiga.nicovideo.jp/image/source/6020286';
+			var thx3 = 'http://seiga.nicovideo.jp/image/source/6019149';
+			var thx4 = 'http://seiga.nicovideo.jp/image/source/5866228';
+			test(thx2,function(){
+				test(thx3,function(){
+					test(thx4)
+				})
+			});
+			
 		});
 }
 
 function auth(login, options){
 	return new Promise(function(resolve, reject){
-		var optionData = querystring.stringify(options);
-		var contentLength = optionData.length;
-		var n = {
-				url : login,
-				form : options
-			};
-		request.post(
-			n,
-			(err,res,body) => {
-				console.log(err);
+		request.post({url : login ,form : options},(err,res,body) => {
 				if(err){
 					console.log(err);
 				}
 				else{
 					var vanilla = cookie.serialize(res.headers['set-cookie']);
-					console.log(vanilla)
 					resolve(vanilla);
 				}
 			}
@@ -46,31 +45,36 @@ function auth(login, options){
 function storecookie(jar){
 	return new Promise(function(resolve, reject){
 		this.jar = jar;
-		//console.log(jar);
-		resolve(jar);
+		resolve();
 	});
 }
 
-function test(str){
-
-	var thx = 'http://live.nicovideo.jp/api/getplayerstatus/nsen/vocaloid';
-	var thx2 = 'http://seiga.nicovideo.jp/image/source/6020286';
-		
-	request({
-		uri : thx2,
-		method : 'get',
+function test(url,next=null){
+	var option = {
 		headers : {
 			Cookie : this.jar
-		}
-	} , function (err,res,body) {
-		console.log(body);
-		
+		},
+		url : url,
+		method : 'get'
+	};
+	request(option , (err,res,body) => {
+
 		if(!err){
 			var $ = cheerio.load(body);
 	  		var title = $('title')[0].children[0].data;
+	  		
 	  		title = title.substring(0,title.lastIndexOf('-')-1);
-	  		var url = 'http://' + res.socket._host + $('img')[1].attribs.src;
+
+	  		var tmptitle = title.replace(/\ /g,'').replace('\n','');
+	  		console.log('['+tmptitle+']');
+	  		if(tmptitle == "ログイン"){
+	  			return ; // error occurs
+	  			// needs to retry
+	  		}
+
+	  		var url = 'http://' + res.socket._host + $('img')[1].attribs.src;	  		
 	  		storeImg(url,title);
+	  		if(next) next();
 		}
 
 	});
@@ -84,7 +88,7 @@ function storeImg(url, title){
 			var filename = __dirname + '/Storage/' + title + '.' + type
 			
 			fs.writeFile(filename, body, 'binary', err => {
-				console.log(err);
+				if(err) console.log('Error to Store : '+ url + ',' + err);
 			});
 		} 
 		else {
